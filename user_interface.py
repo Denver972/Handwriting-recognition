d889597fc55f9daee673c690f54ef0ad4f81f432
Denver972 <150245381+Denver972@users.noreply.github.com>
2024-03-16 21:48:26 +0100

    trying different ui

--- user_interface.py
+++ user_interface.py
@@ -1,8 +1,4 @@
-# User interface file to make the program easy to use without needing to go
-# into the code. The aim is to have a space to specify the input folder, an
-# output folder initially Also there my be toggles to specify if there is
-# debugging or intermediatee steps saved and shown. Additionally a progress bar
-# will be included
+# UI for handwriting recognition
 
 """Modules to build a ui"""
 import tkinter
@@ -15,64 +11,45 @@ customtkinter.set_default_color_theme("dark-blue")
 app = customtkinter.CTk()
 app.title("Handwriting Recognition Tool")
 app.geometry("750x500")
+# app.grid_columnconfigure(0, weight=1)
+app.grid_rowconfigure(0, weight=1)
 
-# this section creates tables
-tabView = customtkinter.CTkTabview(app)
-tabView.add("Settings")
-tabView.add("Extra1")
-tabView.add("Extra2")
-tabView.set("Settings")
 
-# # frame template
-frame = customtkinter.CTkFrame(
-    master=app, width=50, height=50, corner_radius=0, bg_color="green")
-frame2 = frame = customtkinter.CTkFrame(
-    master=app, width=50, height=50, corner_radius=0, bg_color="green")
+# Labels:
 
-# This section creates Text Boxes
-# Making two text boxes to tike the input and output file
-text_in_folder = customtkinter.CTkTextbox(frame)
-text_out_folder = customtkinter.CTkTextbox(app)
-
+# Text boxes: Will be to the bottoma as may depend on frames etc
 IN_TEXT = """
     Input file path here
 """
 OUT_TEXT = """
     Output file path here
 """
-text_in_folder.insert("0.0", text=IN_TEXT)
-text_out_folder.insert("0.0", text=OUT_TEXT)
-text_in_folder.configure(state="disabled")
+text_in_folder = customtkinter.CTkEntry(
+    app, width=200, height=20, placeholder_text=IN_TEXT)
+text_out_folder = customtkinter.CTkEntry(
+    app, width=200, height=20, placeholder_text=OUT_TEXT)
+
+
+# text_in_folder.insert("0.0", text=IN_TEXT)
+# text_out_folder.insert("0.0", text=OUT_TEXT)
+# text_in_folder.configure(state="disabled")
 text_out_folder.configure(state="disabled")
 
-
-# # Button template
-
-
-def button_func():
-    """This code will excecute when the button is pressed"""
-    print(text_in_folder.get("0.0", "end"))
+# Buttons
 
 
-def settings_button_func():
-    """Save the settings before running the program"""
-    print("Settings Saved")
+def button_callback():
+    print(text_in_folder.get())
 
 
 button = customtkinter.CTkButton(
-    master=frame, text="Start Digitising", command=button_func)
+    app, text="Run", command=button_callback)
 
-settings_button = customtkinter.CTkButton(
-    master=tabView.tab("Settings"), text="Save Settings",
-    command=settings_button_func)
 
-# This section populates the buttons, tables and frames
+# Place UI elements
+text_in_folder.grid(row=1, column=0, padx=10, pady=10)
+text_out_folder.grid(row=1, column=2, padx=10, pady=10)
+button.grid(row=2, column=1, padx=20, pady=20)
 
-text_in_folder.pack(padx=10, pady=10)
-tabView.pack(padx=0, pady=10)
-frame.pack(padx=10, pady=10)
-frame2.pack(padx=10, pady=10)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-settings_button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
 app.mainloop()
