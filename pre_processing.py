0045b03bbe02b8721f4c7b883efd6c0f54d921c9
Denver972 <150245381+Denver972@users.noreply.github.com>
2024-03-18 09:22:37 +0100

    formatting

--- pre_processing.py
+++ pre_processing.py
@@ -1,16 +1,17 @@
 # Image preprocessing. The goal is to first tilt the image so text runs
-# horizontally. Then blur the text to merge the text in each column together
-# create bounding boxes around each column. Then potentially sharpen the
-# image and within each bounding box, put bounding boxes around each character.
-# These characters will be indexed with the column number, line they occupy,
-# and the column they occupy. This should allow for the format to be
-# reconstructed after handwriting recognition has been performed.
+# horizontally. Then blur the text to merge the text in each column
+# together create bounding boxes around each column. Then potentially
+# sharpen the image and within each bounding box, put bounding boxes
+# around each character. These characters will be indexed with the
+# column number, line they occupy, and the column they occupy. This
+# should allow for the format to be reconstructed after handwriting
+# recognition has been performed.
 
 class PreProcess():
     """
-    This class will include all the methods required to pre process an image
-    for handwriting recognition. Each method in the class should be called in
-    order to identify where the characters are.
+    This class will include all the methods required to pre process an 
+    image for handwriting recognition. Each method in the class should 
+    be called in order to identify where the characters are.
     """
 
     def __init__(self, file_path):
