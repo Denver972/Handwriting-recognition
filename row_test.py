558b96e296d3a58a7a9d4ef570427b0eb0f9f808
Denver972 <150245381+Denver972@users.noreply.github.com>
2024-06-16 18:19:44 +0200

    Tokenizer finished

--- row_test.py
+++ row_test.py
@@ -1,13 +1,11 @@
 # import numpy as np
-import pandas as pd
-# import cv2 as cv
-# import os
-import timeit
+# import pandas as pd
+# import timeit
 # import time
-from row_pre_processing import PreProcess
-from data_augmentation import DataAugmentation
-from tokenizer import Tokenize
-# import fitz
+# from row_pre_processing import PreProcess
+# from data_augmentation import DataAugmentation
+# from tokenizer import Tokenize
+
 
 ### PRE PROCESS###
 # FILE = "MW1959.pdf"
@@ -25,7 +23,9 @@ FILE = "RowDataset.csv"
 # df = pd.read_csv(FILE)
 # unique_words = df["Label"].unique()
 # print(unique_words)
-test = Tokenize(FILE)
-lst = ["M", "0", "0"]
-num = [test.char_to_number[i] for i in lst]
-print(num)
+# test = Tokenize(FILE)
+# lst = ["M", "0", "0"]
+# num = [test.char_to_number[i] for i in lst]
+# print(num)
+# test.create_csv("TestRow.csv")
+#########################
--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,7 @@
 # Tokenize the words and characters to prep for classification
-
+"""
+Class with methods to create the tokens for
+"""
 
 import pandas as pd
 
@@ -9,8 +11,8 @@ class Tokenize():
     Creates word tokens and character tokens for the dataset
         INPUT: csv file containing the hand labeled words
         OUTPUT: word tokens and character tokens
-        Special words/characters: CIRCLE represents a circle split into quadrants->98
-                                  EMPTY represents an empty cell -> 99
+        Special words/characters: ->64/@
+                                  EMPTY represents an empty cell -> 
     """
 
     def __init__(self, file, pad_value=0):
@@ -22,10 +24,27 @@ class Tokenize():
         self.df = pd.read_csv(self.file)
         self.unique_words = self.df["Label"].unique()
         self.max_word_length = self.get_max_word_length()
+        self.word_tokens = self.get_word_token()
+        self.word_to_number = self.word_to_token()
+        self.number_to_word = self.token_to_word()
         self.characters = self.get_allowed_characters()
         self.char_tokens = self.get_char_token()
         self.char_to_number = self.char_to_token()
         self.number_to_char = self.token_to_char()
+
+    def create_csv(self, file_name):
+        """
+        INPUT: File name of the csv that will be outputed
+        OUTPUT: Csv containing the path and tokens required for classification
+        """
+        new_df = self.df.copy()
+
+        new_df["Class"] = new_df["Label"].map(self.word_to_number)
+        new_df["CharList"] = new_df["Label"].apply(self.word_to_char_list)
+        new_df["TokenList"] = new_df["CharList"].apply(
+            self.char_list_to_token_list)
+
+        new_df.to_csv(file_name, index=False)
 
     def get_max_word_length(self):
         """
@@ -36,13 +55,34 @@ class Tokenize():
         for word in self.unique_words:
             if len(word) > self.max_word_length:
                 self.max_word_length = len(word)
-
         return self.max_word_length
 
     def get_word_token(self):
         """
-        Asign a number or "token" to each unique word.
+        Asign a number or "token" to each unique word
         """
+        number = []
+        for ix, word in enumerate(self.unique_words):
+            number.append(ix)
+
+        return number
+
+    def word_to_token(self):
+        """
+        convert the words to a number 
+        """
+        classes = dict(
+            map(lambda i, j: (i, j), self.unique_words, self.word_tokens))
+
+        return classes
+
+    def token_to_word(self):
+        """
+        Convert from the number to the word
+        """
+        inv_classes = {i: j for j, i in self.word_to_number.items()}
+
+        return inv_classes
 
     def get_allowed_characters(self):
         """
@@ -50,12 +90,12 @@ class Tokenize():
         Remove the special words ENPTY and CIRCLE from the unique list
         before passing to the character level as these characters may not
         be present in the dataset. Then sort the characters by their ascii
-        value
+        value. Let @ represent the CIRCLE in a single character
         """
         characters = []
         modified_unique_words = self.unique_words.copy()
         modified_unique_words = modified_unique_words[modified_unique_words != "EMPTY"]
-        modified_unique_words = modified_unique_words[modified_unique_words != "CIRCLE"]
+        # modified_unique_words = modified_unique_words[modified_unique_words != "CIRCLE"]
 
         modified_unique_words = "".join(map(str, modified_unique_words))
         characters = list(set(modified_unique_words))
@@ -64,7 +104,7 @@ class Tokenize():
 
     def get_char_token(self):
         """
-        Asign a number or "token" to each unique word. Use the ASCII value
+        Asign a number or "token" to each unique char. Use the ASCII value
         for the letters. Bounded by 32 and 126. Need to come up with a way to 
         symbolise an empty charatcter and the circle character. Let empty 
         character_token be 0 and circle be 1
@@ -89,6 +129,54 @@ class Tokenize():
         convert the token back into the character. Just takes the inverse
         of the char_to_token dictionary
         """
-        inv_classes = {v: k for k, v in self.char_to_number.items()}
+        inv_classes = {i: j for j, i in self.char_to_number.items()}
 
         return inv_classes
+
+    def word_to_char_list(self, word):
+        """
+        Converts the word to a list of characters
+        """
+        char_list = list(word)
+        return char_list
+
+    def char_list_to_word(self, char_list):
+        """
+        Convert a list of characters to a string 
+        """
+        word = "".join(char_list)
+        return word
+
+    def char_list_to_token_list(self, char_list):
+        """
+        Converts the list of characters to a list of tokens and adds padding
+        INPUT: List of characters
+        OUTPUT: List of tokens
+        Note ascii of @ is 64
+        """
+
+        if char_list == ["E", "M", "P", "T", "Y"]:
+            token_list = [0]
+        else:
+            token_list = [self.char_to_number[i] for i in char_list]
+
+        while self.max_word_length - len(token_list) > 0:
+            token_list.append(0)
+
+        return token_list
+
+    def token_list_to_char_list(self, token_list):
+        """
+        Converts the list of characters to a list of tokens and adds padding
+        INPUT: List of tokens
+        OUTPUT: List of characters with padding removed
+        """
+
+        if token_list == [0]*self.max_word_length:
+            char_list = ["E", "M", "P", "T", "Y"]
+        else:
+            # remove the padding value of 0
+            mod_token_list = [i for i in token_list if i != 0]
+            char_list = [self.number_to_char[i] for i in mod_token_list]
+
+        return char_list
